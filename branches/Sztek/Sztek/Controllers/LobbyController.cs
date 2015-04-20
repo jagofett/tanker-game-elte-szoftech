c3c7ebae9ce63aa4d909ec8ffba7e271f9ac856c
Akos <magas.akos@gmail.com>
2015-04-20 17:17:47 +0200

    new lobby added

---
+++ branches/Sztek/Sztek/Controllers/LobbyController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SignalRChatApp.Hubs;
+using Sztek.Models;
+
+namespace Sztek.Controllers
+{
+    public class LobbyController : Controller
+    {
+        private readonly DatabaseEntities _entities = new DatabaseEntities();
+        private readonly HubHandler _hubHandler;
+
+        public ActionResult JoinLobby()
+        {
+            return View();
+        }
+        public void GetLobbyList()
+        {
+            
+        }
+
+
+        public JsonResult JoinedToLobby()
+        {
+            return new JsonResult();
+        }
+
+        private Object LobbyUsers()
+        {
+            return null;
+        }
+
+        public ActionResult Lobby()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var current = _entities.Users.FirstOrDefault(us => us.username == User.Identity.Name);
+                if (current == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ViewBag.Message = current.in_lobby.GetValueOrDefault()
+                    ? "Kilépés"
+                    : "Csatlakozás";
+
+                return View(_entities);
+            }
+            else
+            {
+                return RedirectToAction("NotLoggedIn", "Home");
+            }
+        }
+    }
+}
---
+++ branches/Sztek/Sztek/Models/newLobby.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sztek.Models
+{
+    public class newLobby
+    {
+        public int id { get; set; }
+        public string lobbyName { get; set; }
+        public bool isMainLobby { get; set; }
+        public virtual games game { get; set; }
+
+        public virtual List<users> UserList { get; set; } 
+
+
+    }
+}
