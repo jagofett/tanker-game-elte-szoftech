3814c62db3db7b665c501455f6806164764d0f8f
tamasszadvari <tamas.szadvari@gmail.com>
2015-05-10 22:20:33 +0200

    Help needed

--- branches/Sztek/Sztek/Controllers/LobbyController.cs
+++ branches/Sztek/Sztek/Controllers/LobbyController.cs
@@ -65,12 +65,12 @@ namespace Sztek.Controllers
                         x.inLobby = false;
                         x.game = newGame;
                     });
+                    
+                    _entities.Games.First(x => x.id == gid.id).gameName = gid.gameType == 0
+                        ? "DeathMatch" + gid.id
+                        : "TeamDeathMatch" + gid.id;
+
                     //egyéb game  indítás...
-                    _entities.SaveChanges();
-                    var currentGame = _entities.Games.Last();
-                    currentGame.gameName = currentGame.gameType == 0
-                        ? "DeathMatch" + currentGame.id
-                        : "TeamDeathMatch" + currentGame.id;
                     _entities.SaveChanges();
 
                     //soap hívás, server instance létrehozása
@@ -84,6 +84,62 @@ namespace Sztek.Controllers
                     GetLobbyList();
                     id = newGame.id;
                 }
+                error = false;
+
+                btnString = current.inLobby.GetValueOrDefault()
+                    ? "Kilépés"
+                    : "Csatlakozás";
+            }
+
+            return Json(new { error = error, btnStr = btnString }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult JoinLobby(int gameId)
+        {
+            var current = _entities.Users.FirstOrDefault(us => us.username == User.Identity.Name);
+            if (current == null)
+                return null;
+
+            int? id = null;
+            var error = true;
+            string btnString = "Csatlakozás";
+
+            if (current.game == null)
+            {
+                current.inLobby = !current.inLobby.GetValueOrDefault();
+                _entities.SaveChanges();
+                //send the new lobby to users
+                GetLobbyList();
+                //if the lobby is full, start the game - todo new method!!
+                var inLobby = _entities.Users.Where(x => x.inLobby != null && (bool)x.inLobby).ToList();
+                /*if (inLobby.Count() >= 2)
+                {
+                    var gid = _entities.Games.Add(newGame);
+                    inLobby.ForEach(x =>
+                    {
+                        x.inLobby = false;
+                        x.game = newGame;
+                    });
+
+                    _entities.Games.First(x => x.id == gid.id).gameName = gid.gameType == 0
+                        ? "DeathMatch" + gid.id
+                        : "TeamDeathMatch" + gid.id;
+
+                    //egyéb game  indítás...
+                    _entities.SaveChanges();
+
+                    //soap hívás, server instance létrehozása
+                    var port = (newGame.id % 10000) + 10000;
+                    var users = inLobby.Select(users1 => users1.id.ToString()).ToArray();
+                    //var proxy = new MainClient();
+                    //proxy.startGameServer(port.ToString(), users[0],users[1], users[2], users[3]);
+
+                    var userList = inLobby.Select(x => new { x.username, userId = x.id, gameId = newGame.id });
+                    _hubHandler.StartGame(userList);
+                    GetLobbyList();
+                    id = newGame.id;
+                }*/
                 error = false;
 
                 btnString = current.inLobby.GetValueOrDefault()
@@ -118,13 +174,19 @@ namespace Sztek.Controllers
 
         private Object ActiveGamesList()
         {
-            var games = _entities.Games;
-            var actives = games.Where(game => game.status != null && (bool)game.status)
+            var actives = _entities.Games.Where(game => game.status != null && (bool)game.status)
                         .OrderBy(game => game.id)
-                        .Select(x => new { x.gameName, x.gameType })
+                        .Select(x => new { x.id, x.gameName, x.gameType, 
+                            hasCurrentUser = x.users.Contains(_entities.Users.FirstOrDefault(us => us.username == User.Identity.Name))
+                        })
                         .ToList();
 
             return actives;
+        }
+
+        public int GetCurrentUserId()
+        {
+            return _entities.Users.FirstOrDefault(us => us.username == User.Identity.Name).id;
         }
 
         public ActionResult Lobby()
