d4daba5fb1d1a6fccda634e895ab39041e3d3e1a
Akos <magas.akos@gmail.com>
2015-05-10 15:51:24 +0200

    usergame adsed

---
+++ branches/Sztek/Sztek/Models/userGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sztek.Models
+{
+    public class userGame
+    {
+        public int id { get; set; }
+        public virtual games game { get; set; }
+        public virtual users user { get; set; }
+        public  int team { get; set; }
+
+
+    }
+}
